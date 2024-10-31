a2e75f2c988f3faf8866d5590aaf8f0d71418453
annz <finnj9332@gmail.com>
2024-10-31 16:25:36 +0800

    basic structure of the code and a print hello world statement

---
+++ src/main/java/com/mycompany/gitpractice/GITpractice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitpractice;
+
+/**
+ *
+ * @author Acer
+ */
+public class GITpractice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
