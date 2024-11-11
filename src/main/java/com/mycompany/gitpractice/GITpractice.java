4c2f9126c9f076cf8d9f08ee8cd1135bb79b8b1d
annz <finnj9332@gmail.com>
2024-11-11 16:58:39 +0800

    Pattern

--- src/main/java/com/mycompany/gitpractice/GITpractice.java
+++ src/main/java/com/mycompany/gitpractice/GITpractice.java
@@ -8,9 +8,60 @@ package com.mycompany.gitpractice;
  *
  * @author Acer
  */
+import java.util.Scanner;
 public class GITpractice {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Title: THE partern shit");
+        System.out.print("Enter number: ");
+        int num = s.nextInt();
+        
+        //all(num);
+        upsidedown_triangle(num);
+        
+        
+        System.out.println("shtshtshtsht");
+        
+    }
+    public static void descending(int num){
+        for(int i  = 1; i <= num ; i++){
+            for(int j = num; j >= i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void ascending(int num){
+        for(int i  = 1; i <= num; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void all(int num) {
+        descending(num);
+        ascending(num);
+    }
+    
+    
+    public static void upsidedown_triangle(int num){
+        long startTime  = System.nanoTime();
+        for (int i = 1; i <= num; i++){
+            for (int j = num; j >= i; j--){
+                System.out.print(" ");
+            }
+            
+            for (int h = 1; h<=i; h++){
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+        long duration = System.nanoTime() - startTime;
+
+        System.out.println("Runtime: " + duration + " nanoseconds");
     }
 }
