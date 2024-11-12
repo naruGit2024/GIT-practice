b97a7553dc8ed9ff4d964d87ff404c381877700b
annz <finnj9332@gmail.com>
2024-11-12 16:48:05 +0800

    rename the howllow_box function

--- src/main/java/com/mycompany/gitpractice/GITpractice.java
+++ src/main/java/com/mycompany/gitpractice/GITpractice.java
@@ -23,7 +23,7 @@ public class GITpractice {
         
         System.out.println("shtshtshtsht");
         
-        hollow_box(num);
+        mother(num);
         
     }
     public static void descending(int num){
@@ -67,7 +67,7 @@ public class GITpractice {
         System.out.println("Runtime: " + duration + " nanoseconds");
     }
     
-    public static void hollow_box(int num){
+    public static void mother(int num){
         for(int i = 1; i<= num; i++){
             for (int j = 1; j<=num; j++){
                 if (i==num || j == num || i==1|| j ==1 ){
